aa511fae0e7a46cce27c5e665686e2d832481364
MarkGraebener <mark.w.graebener@gmail.com>
2022-01-14 16:36:41 -0600

    up to styling

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 "use strict"
 //for each loop to find selected input
 function renderCoffee(coffee) {
-    let html = '<div class="col-6 my-3">';
+    let html = '<div class="col-5 my-3">';
     // html += '<td>' + coffee.id + '</td>';
-    html += '<div class="row border">'
-    html += '<h3 class="col-8">' + coffee.name + '</h3>';
-    html += '<p class="col-4 border text-muted ">' + coffee.roast + '</p>';
+    html += '<div class="row ">'
+    html += '<h3 class="col-md-7">' + coffee.name + '</h3>';
+    html += '<p class="col-md-3  text-muted ">' + coffee.roast + '</p>';
     html += '</div>'
     html += '</div>';
 
@@ -14,12 +14,12 @@ function renderCoffee(coffee) {
 
 function renderCoffees(coffees) {
     let html = '';
-    for(let i = coffees.length - 1; i >= 0; i--) {
+    for(let i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
-
+//displays selected roast from dropdown from first form
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     let selectedRoast = roastSelection.value;
@@ -29,11 +29,15 @@ function updateCoffees(e) {
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
+            display.innerHTML = renderCoffees(filteredCoffees);
+        }else if (selectedRoast === "All"){
+            display.innerHTML = renderCoffees(coffees);
+
         }
     });
-    display.innerHTML = renderCoffees(filteredCoffees);
-}
 
+}
+//displays searched coffees in first form
 function updateCoffeesNames(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     let selectedCoffee = coffeeName.value;
@@ -48,28 +52,27 @@ function updateCoffeesNames(e) {
     });
     display.innerHTML = renderCoffees(filteredCoffees);
 }
-
+//adds coffee and roast to coffees obj. and displays new obj. coffees
 function addCoffee(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    let newCoffee = document.getElementById("addCoffee");
-    let newRoast = document.getElementById("roastSelector2");
+    let newCoffee = document.getElementById("addCoffee").value;
+    let newRoast = document.getElementById("roastSelector2").value;
     // coffees.push(`name: ${newCoffee} roast: ${newRoast}`)
-    Object.assign(coffees, {key3: "value3",name: newCoffee, roast: newRoast});
+    let newCoffeeObj ={
+        id: coffees.length + 1,
+        name: newCoffee,
+        roast: newRoast,
+    }
+    coffees.push(newCoffeeObj)
+    // Object.assign(coffees, {key3: "value3",name: newCoffee, roast: newRoast,});
     console.log(coffees);
-    display.innerHTML = renderCoffees();
+    // console.log(newCoffeeObj);
+    console.log(newCoffee);
+    display.innerHTML = renderCoffees(coffees);
 
 }
-//grab user input as they type
-//compair user input to coffe name
-// filter through coffee names for matching chars in input
-
-//buttonElement.addEventListener('click', {
-//   handleEvent: function (event) {
-//     handleEvtOutput.textContent = 'Element clicked through handleEvent property!';
-//   }
-
-
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+//data base of coffees
 let coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
@@ -86,7 +89,7 @@ let coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
-
+// sets vars from selected id's
 let display = document.querySelector('#coffees');
 let submitButton = document.querySelector('#submit');
 let submitButton2 = document.querySelector('#submit2');
@@ -94,7 +97,12 @@ let roastSelection = document.querySelector('#roast-selection');
 let coffeeName = document.querySelector("#coffeeName");
 
 display.innerHTML = renderCoffees(coffees);
-
+// eventlisteners section
 submitButton.addEventListener('click', updateCoffees);
 document.getElementById("coffeeName").addEventListener('keyup', updateCoffeesNames);
 submitButton2.addEventListener('click', addCoffee);
+
+//buttonElement.addEventListener('click', {
+//   handleEvent: function (event) {
+//     handleEvtOutput.textContent = 'Element clicked through handleEvent property!';
+//   }
